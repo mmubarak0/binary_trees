015ecb560a3288712815e3fcf00d8e5f10c05cc7
Mohammed Mubarak <modymu9@gmail.com>
2023-11-01 15:41:22 +0200

    handle failure of memory allocation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,9 +10,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 	if (parent == NULL)
 	{
-		new_node = malloc(sizeof(binary_tree_t));
 		new_node->n = value;
 		new_node->parent = NULL;
 		new_node->left = NULL;
@@ -20,7 +22,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	else if (parent->left == NULL)
 	{
-		new_node = malloc(sizeof(binary_tree_t));
 		new_node->n = value;
 		new_node->parent = parent;
 		new_node->left = NULL;
@@ -29,7 +30,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	else if (parent->right == NULL)
 	{
-		new_node = malloc(sizeof(binary_tree_t));
 		new_node->n = value;
 		new_node->parent = parent;
 		new_node->left = NULL;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,9 +13,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 	if (parent->right == NULL)
 	{
-		new_node = malloc(sizeof(binary_tree_t));
 		new_node->n = value;
 		new_node->parent = parent;
 		new_node->left = NULL;
@@ -24,7 +26,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		new_node = malloc(sizeof(binary_tree_t));
 		new_node->n = value;
 		new_node->parent = parent;
 		new_node->left = NULL;
